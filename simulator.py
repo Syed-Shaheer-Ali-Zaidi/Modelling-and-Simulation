7e62cc02581b9463e97575886600b35b068fb061
Syed-Shaheer-Ali-Zaidi <shaheerinkhi@gmail.com>
2023-10-01 15:37:23 +0500

    Code is now Modular

--- simulator.py
+++ simulator.py
@@ -19,7 +19,7 @@ def CP(Lambda):
         num_of_cust+=1
     return array, num_of_cust
 
-def CPlook(Lambda, num_of_cust):
+def CPlookUp(Lambda, num_of_cust):
     array = []
     for i in range (0, num_of_cust):
         total = 0
@@ -29,27 +29,27 @@ def CPlook(Lambda, num_of_cust):
         array.append(total)
     return array
 
-arr1, num_of_cust = CP(Lambda)
-arr2 = CPlook(Lambda, num_of_cust)
-IA = []
+def InterArrival(CP, CPlo, num_of_cust):
+    IA = []
+    for j in range (1, num_of_cust):
+        temp = random.random()
+        for i in range (0, num_of_cust - 1):
+            if (temp<CP[i] and temp>CPlo[i]):
+                IA.append(i)
+    return IA
 
-for j in range (1, num_of_cust):
-    temp = random.random()
+def Arrivals(arrivals, IA, num_of_cust):
+    temp = 0
     for i in range (0, num_of_cust - 1):
-        if (temp<arr1[i] and temp>arr2[i]):
-            IA.append(i)
+        temp += IA[i]
+        arrivals.append(temp)
 
-arrivals = [0]
-temp = 0
-for i in range (0, num_of_cust - 1):
-    temp += IA[i]
-    arrivals.append(temp)
-IA.insert(0, 0)
-
-service = []
-for i in range (0, num_of_cust):
-    temp = -mew * math.log(random.random())
-    service.append(round(temp))
+def Service(num_of_cust):
+    service = []
+    for i in range (0, num_of_cust):
+        temp = -mew * math.log(random.random())
+        service.append(round(temp))
+    return service
 
 def generate_priority(A, M, Z0, C, a, b, num_of_cust):
     Z = [Z0]
@@ -65,3 +65,15 @@ def generate_priority(A, M, Z0, C, a, b, num_of_cust):
         GP.append(round(priority))
     Z.remove(Z[-1])
     return Z, R, RanNum, GP
+
+def main():
+    arr1, num_of_cust = CP(Lambda)
+    arr2 = CPlookUp(Lambda, num_of_cust)
+    IA = InterArrival(arr1, arr2, num_of_cust)
+    arrivals = [0]
+    Arrivals(arrivals, IA, num_of_cust)
+    IA.insert(0, 0)
+    service = Service(num_of_cust)
+
+if __name__ == "__main__":
+    main()
