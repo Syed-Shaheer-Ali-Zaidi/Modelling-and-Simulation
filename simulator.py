c93b7137441bbb8d1c8d6dc08fe7116a4b7d7d2e
Syed-Shaheer-Ali-Zaidi <shaheerinkhi@gmail.com>
2023-10-03 20:12:31 +0500

    MM1 Queing added

--- simulator.py
+++ simulator.py
@@ -1,8 +1,15 @@
 import math
 import random
+from prettytable import PrettyTable
 
-mew = 8.98
 Lambda = 2.25
+mew = 8.98
+A = 55
+M = 1994
+Z0 = 10112166
+C = 9
+a = 1
+b = 3
 
 def CP(Lambda):
     array = []
@@ -66,6 +73,123 @@ def generate_priority(A, M, Z0, C, a, b, num_of_cust):
     Z.remove(Z[-1])
     return Z, R, RanNum, GP
 
+def qeueing(num_of_cust, arrivals, service):
+  
+  Z, R, RanNum, GP = generate_priority(A, M, Z0, C, a, b, num_of_cust)
+  arrived = []
+  for i in range(len(arrivals)):
+        arrived.append({
+        "name": f"Process {i + 1}",
+        "arrival_time": arrivals[i],
+        "service_time": service[i],
+        "priority": GP[i],
+    })
+
+
+  table = PrettyTable([
+    "Name", "Arrival Time", "Service Time", "Priority", "Service Start Time",
+    "Service End Time", "Turnaround Time", "Wait Time", "Response Time"
+        ])
+  time = 0
+  n = len(arrived)
+  executed = 0
+  current = 0
+  waiting_queue = []
+  executed_processes = set()
+  remaining_times = {
+      p["name"]: [p["service_time"], p["priority"], None, 0]
+      for p in arrived
+  }
+  total_service_time = 0
+  total_busy_time = 0
+
+  while executed < n:
+    for p in arrived:
+      if p["arrival_time"] == time and p["name"] not in executed_processes:
+        waiting_queue.append(p)
+
+        if (not current) or (waiting_queue and remaining_times[p["name"]][1] < remaining_times[current["name"]][1]):
+          if current:
+            print(
+                "\n"
+                f"Leaving {current['name']} and Switching to process {p['name']} due to priority."
+                "\n")
+          current = p
+
+    if not current and waiting_queue:
+      current = min(waiting_queue, key=lambda x: remaining_times[x["name"]][1])
+
+    if not current and not waiting_queue:
+      print("\n"
+            f"Time {time}: Server is idle."
+            "\n")
+
+    if current:
+      print(f"Time {time}: Executing {current['name']}")
+
+      if remaining_times[current["name"]][2] == None:
+        remaining_times[current["name"]][2] = time  # Service start time
+      remaining_times[current["name"]][0] -= 1
+      total_service_time += 1
+      total_busy_time += 1
+      if remaining_times[current["name"]][0] <= 0:
+        executed_processes.add(current["name"])
+        remaining_times[current["name"]][3] = time + 1 # Service end time
+        waiting_queue = [
+            p for p in waiting_queue if p["name"] != current["name"]
+        ]
+
+        turnaround_time = remaining_times[
+            current["name"]][3] - current["arrival_time"]
+
+        wait_time = max(turnaround_time - current["service_time"], 0)
+
+        response_time = remaining_times[
+            current["name"]][2] - current["arrival_time"]
+
+        table.add_row([
+            current["name"], current["arrival_time"], current["service_time"],
+            current["priority"], remaining_times[current["name"]][2],
+            remaining_times[current["name"]][3], turnaround_time, wait_time,
+            response_time
+        ])
+
+        current = None
+        executed += 1
+
+    time += 1
+  server_utilization_rate = total_busy_time / time
+
+  print(table)
+  #print("\n")
+  #print(f"\nTotal Service Time: {total_service_time}")
+
+  total_waiting_time = 0
+  total_turnaround_time = 0
+  total_response_time = 0
+
+  for p in arrived:
+    if p["name"] in executed_processes:
+      turnaround_time = remaining_times[p["name"]][3] - p["arrival_time"]
+      wait_time = max(turnaround_time - p["service_time"], 0)
+      response_time = remaining_times[p["name"]][2] - p["arrival_time"]
+
+      total_waiting_time += wait_time
+      total_turnaround_time += turnaround_time
+      total_response_time += response_time
+
+  # Calculate averages
+  average_waiting_time = total_waiting_time / n
+  average_turnaround_time = total_turnaround_time / n
+  average_response_time = total_response_time / n
+
+  # Print the results
+  print("\n")
+  print(f"Average Waiting Time: {average_waiting_time:.2f}")
+  print(f"Average Turnaround Time: {average_turnaround_time:.2f}")
+  print(f"Average Response Time: {average_response_time:.2f}")
+  print(f"\nServer Utilization Rate: {server_utilization_rate * 100:.2f} %")
+
 def main():
     arr1, num_of_cust = CP(Lambda)
     arr2 = CPlookUp(Lambda, num_of_cust)
@@ -74,6 +198,7 @@ def main():
     Arrivals(arrivals, IA, num_of_cust)
     IA.insert(0, 0)
     service = Service(num_of_cust)
+    qeueing(num_of_cust, arrivals, service)
 
 if __name__ == "__main__":
     main()
